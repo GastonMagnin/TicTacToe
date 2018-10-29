0b09ad44d1c3837aa7d1bcf1d50929cb4fcf65f5
GastonMagnin <44193118+GastonMagnin@users.noreply.github.com>
2018-10-29 18:55:53 +0100

    v.1.0.4

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-<<<<<<< HEAD
+	public class myBoolean {
+		public boolean value;
 
-=======
->>>>>>> 1b76c912cc5922a186098df66504861e8ce9992e
+		myBoolean(boolean val) {
+			this.value = val;
+		}
+	}
+
 	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+		game.run();
+	}
+
+	public void run() {
+		// Create Scanner
 		Scanner sc = new Scanner(System.in);
 		String input = "";
 		do {
@@ -19,70 +29,86 @@ public class TicTacToe {
 		}
 		int rows = 0;
 		do {
-			rows = getNextInt(sc, "Wieviele Zeilen soll das Spielfeld haben?\nBitte geben sie eine ganze Zahl ueber 0 ein");
+			rows = getNextInt(sc,
+					"Wieviele Zeilen soll das Spielfeld haben?\nBitte geben sie eine ganze Zahl ueber 0 ein");
 		} while (rows <= 0);
 		int columns = 0;
 		do {
-			columns = getNextInt(sc, "Wieviele Spalten soll das Spielfeld haben?\nBitte geben sie eine ganze Zahl ueber 0 ein");
+			columns = getNextInt(sc,
+					"Wieviele Spalten soll das Spielfeld haben?\nBitte geben sie eine ganze Zahl ueber 0 ein");
 		} while (columns <= 0);
 
-		int[][][] container = generatePosibilities(rows, columns);
+		// Initialize variables
+		myBoolean playing = new myBoolean(true);
+		myBoolean checkHorizontal = new myBoolean(true);
+		myBoolean checkVertical = new myBoolean(true);
+		myBoolean checkDiagonal = new myBoolean((rows == columns) ? true : false);
+		int[][] field = new int[rows][columns];
+		int[][] rowsPosibilities = new int[rows][columns];
+		int[][] columnsPosibilities = new int[columns][rows];
+		generatePosibilities(rows, columns, rowsPosibilities, columnsPosibilities);
 
 		// TODO add CPU opponent
 		boolean currentPlayer = true;
 		int inputRow;
+		int inputColumn;
 		do {
-			printField(container);
+			printField(field);
 			boolean valid = false;
 			do {
 				try {
-					System.out.println((currentPlayer ? "Spieler1" : "Spieler2") + " ist am Zug"); 
-					inputRow = getNextInt(sc, "Geben sie die Zeile ihrer Auswahl ein");
-					container[0][inputRow + 1][getNextInt(sc, "Geben sie die Spalte ihrer Auswahl ein") + 1] = (currentPlayer) ? 1 : 2;
-					checkPosibilities(container);
+					System.out.println((currentPlayer ? "Spieler1" : "Spieler2") + " ist am Zug");
+					inputRow = getNextInt(sc, "Geben sie die Zeile ihrer Auswahl ein") - 1;
+					inputColumn = getNextInt(sc, "Geben sie die Spalte ihrer Auswahl ein") - 1;
+					if(field[inputRow][inputColumn] == 0) {
+						field[inputRow][inputColumn] = (currentPlayer)? 1 : 2;
+					}else {
+						System.out.println("Dieses Feld ist bereits besetzt bitte geben sie ein leeres Feld ein");
+						continue;
+					}
+					
+					checkPosibilities(field, playing, checkHorizontal, checkVertical, checkDiagonal,
+							columnsPosibilities, rowsPosibilities);
 					currentPlayer = !currentPlayer;
 					valid = true;
 				} catch (java.lang.ArrayIndexOutOfBoundsException e) {
 					System.out.println("Bitte geben sie einen Wert ein der innerhalb des Spielfelds liegt");
 				}
 			} while (!valid);
-		} while (container[3][0][0] != 0);
+		} while (playing.value);
 
 		sc.close();
 	}
 
-	public static int[][][] generatePosibilities(int rows, int columns) {
-		int[][] columnsPosibilities = new int[columns][rows];
+	public static void generatePosibilities(int rows, int columns, int[][] rowsPosibilities,
+			int[][] columnsPosibilities) {
+
 		for (int i = 0; i < columnsPosibilities.length; i++) {
 			for (int j = 0; j < rows; j++) {
 				columnsPosibilities[i][j] = j;
 			}
 		}
-		int[][] rowsPosibilities = new int[rows][columns];
+
 		for (int i = 0; i < columnsPosibilities.length; i++) {
 			for (int j = 0; j < columns; j++) {
 				rowsPosibilities[i][j] = j;
 			}
 		}
-		int[][] field = new int[rows][columns];
-		int checkDiagonal = (rows == columns) ? 1 : 0;
-		int[][][] container = new int[][][] { field, columnsPosibilities, rowsPosibilities, { { 1, 1, 1, checkDiagonal } } };
-		return container;
+
 	}
 
-	public static void checkPosibilities(int[][][] container) {
-		int[][] columnsPosibilities = container[1];
-		int[][] rowsPosibilities = container[2];
+	public static void checkPosibilities(int[][] field, myBoolean playing, myBoolean checkHorizontal,
+			myBoolean checkVertical, myBoolean checkDiagonal, int[][] columnsPosibilities, int[][] rowsPosibilities) {
 		boolean rowsLeft = false;
 		boolean columnsLeft = false;
-		
-		if(container[3][0][1] == 1) {
+
+		if (checkVertical.value) {
 			for (int column = 0; column < columnsPosibilities.length; column++) {
 				if (columnsPosibilities[column] == null)
 					continue;
 				String columnString = "";
 				for (int row : columnsPosibilities[column]) {
-					columnString += container[0][row][column];
+					columnString += field[row][column];
 				}
 				if (columnString.contains("1") && columnString.contains("2")) {
 					columnsPosibilities[column] = null;
@@ -92,22 +118,22 @@ public class TicTacToe {
 				if (!columnString.contains("0")) {
 					if (columnString.contains("1")) {
 						System.out.println("Player 1 wins");
-						container[3][0][0] = 0;
+						playing.value = false;
 					} else {
 						System.out.println("Player 2 wins");
-						container[3][0][0] = 0;
+						playing.value = false;
 					}
 				}
 			}
 		}
-		
-		if(container[3][0][1] == 1) {
+
+		if (checkHorizontal.value) {
 			for (int row = 0; row < rowsPosibilities.length; row++) {
 				if (rowsPosibilities[row] == null)
 					continue;
 				String rowString = "";
 				for (int column : rowsPosibilities[row]) {
-					rowString += container[0][row][column];
+					rowString += field[row][column];
 				}
 				if (rowString.contains("1") && rowString.contains("2")) {
 					columnsPosibilities[row] = null;
@@ -117,35 +143,34 @@ public class TicTacToe {
 				if (!rowString.contains("0")) {
 					if (rowString.contains("1")) {
 						System.out.println("Player 1 wins");
-						container[3][0][0] = 0;
+						playing.value = false;
 					} else {
 						System.out.println("Player 2 wins");
-						container[3][0][0] = 0;
+						playing.value = false;
 					}
 				}
 			}
 		}
-		
-		container[1] = columnsPosibilities;
-		container[2] = rowsPosibilities;
-		container[3][0][1] = rowsLeft ? 1 : 0;
-		container[3][0][2] = columnsLeft ? 1 : 0;
-		if(container[3][0][3] != 0) checkDiagonal(container);
-		if ((container[3][0][1] + container[3][0][2] + container[3][0][3]) == 0){
-			container[3][0][0] = 0;
+
+		checkHorizontal.value = rowsLeft;
+		checkVertical.value = columnsLeft;
+		if (checkDiagonal.value)
+			checkDiagonalPosibilities(field, checkDiagonal, playing);
+		if (!(checkHorizontal.value || checkVertical.value || checkDiagonal.value)) {
+			playing.value = false;
 			System.out.println("Draw, there is no posibility to win left");
 		}
 	}
 
-	public static void checkDiagonal(int[][][] container) {
+	public static void checkDiagonalPosibilities(int[][] field, myBoolean checkDiagonal, myBoolean playing) {
 
 		boolean checkDia = true;
 		String diaString = "";
 		String diaString2 = "";
 
-		for (int i = 0, j = container[0][0].length - 1; i < container[0][0].length; i++, j--) {
-			diaString += container[0][i][i];
-			diaString2 += container[0][i][j];
+		for (int i = 0, j = field[0].length - 1; i < field[0].length; i++, j--) {
+			diaString += field[i][i];
+			diaString2 += field[i][j];
 
 		}
 		for (String diagonalString : (new String[] { diaString, diaString2 })) {
@@ -154,38 +179,39 @@ public class TicTacToe {
 			} else if (!diagonalString.contains("0")) {
 				if (diagonalString.contains("1")) {
 					System.out.println("Player 1 wins");
-					container[3][0][0] = 0;
+					playing.value = false;
 				} else {
 					System.out.println("Player 2 wins");
-					container[3][0][0] = 0;
+					playing.value = false;
 				}
 			}
 			checkDia = true;
 		}
 
-		container[3][0][3] = checkDia ? 1 : 0;
+		checkDiagonal.value = checkDia;
 
 	}
 
-	public static void printField(int[][][] container) {
-		for(int i = 0; i < container[0][0].length ; i++) {
+	public void printField(int[][] field) {
+		for (int i = 0; i < field[0].length; i++) {
 			System.out.print("____");
 		}
 		System.out.print("\n");
-		for (int[] row : container[0]) {
+		for (int[] row : field) {
 			System.out.print("| ");
 			for (int entry : row) {
 				System.out.print(entry + " | ");
 			}
 			System.out.print("\n|");
-			for(int i = 0; i < row.length ; i++) {
+			for (int i = 0; i < row.length; i++) {
 				System.out.print("___|");
 			}
 			System.out.print("\n");
 		}
-		
+
 	}
-	public static int getNextInt(Scanner sc, String prompt) {
+
+	public int getNextInt(Scanner sc, String prompt) {
 		boolean valid = false;
 		int input = 0;
 		do {
